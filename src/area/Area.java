98b66937a4a52cd9a392b75fa9f2af4c3630837e
Juan Sebastian Villamizar Bolivar <juans.villamizar@correo.usa.edu.co>
2020-09-06 19:17:11 -0500

    Repositorio

---
+++ src/area/Area.java
@@ -0,0 +1,14 @@
+
+package area;
+
+
+public class Area {
+    public static void main(String[] args) {
+
+        Areatr guiArea = new Areatr();
+        guiArea.setVisible(true);
+        guiArea.setLocationRelativeTo(null);
+        
+    }
+    
+}
